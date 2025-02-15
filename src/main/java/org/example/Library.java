35c6a7d8159995b6330040fa7b7666f5b47ede51
D175F <daniyarjabashev@outlook.com>
2025-02-15 23:12:34 +0500

    Add @Component annotations to Book and Library and @Bean implementation

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Library {
     private String name;
     private Book book;
--- src/main/java/org/example/ProjectConfig.java
+++ src/main/java/org/example/ProjectConfig.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan(basePackages = "org.example")
 public class ProjectConfig {
+
+    @Bean
+    @Primary
+    public Book book1() {
+        Book book = new Book();
+        book.setTitle("1984");
+        return book;
+    }
+
+    @Bean
+    public Book book2() {
+        Book book = new Book();
+        book.setTitle("Harry Potter");
+        return book;
+    }
+
+    @Bean
+    public Book book3() {
+        Book book = new Book();
+        book.setTitle("War and Peace");
+        return book;
+    }
+
+    @Bean
+    public Library library() {
+        Library library = new Library();
+        library.setName("City Library");
+        library.setBook(book2());
+        return library;
+    }
 }
